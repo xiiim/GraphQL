ca839b5182cba33eace7d83781542238c7324071
Eric <849895374@qq.com>
2024-03-26 21:58:45 +1100

    add ruruHTML for graphical interaction with the api

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var { graphql, buildSchema } = require("graphql")
+var express = require('express')
+var { createHandler } = require('graphql-http/lib/use/express')
+var { ruruHTML } = require('ruru/server')
 
 var schema = buildSchema(`
   type Query {
@@ -12,8 +15,15 @@ var rootValue = {
   age: () => 8,
 }
 
-var source = "{ hello, age }"
 
-graphql({ schema, source, rootValue }).then(response => {
-  console.log(response)
+const app = express()
+
+app.all('/graphql', createHandler({ schema, rootValue }))
+
+app.get('/', (_req, res) => {
+  res.type('html')
+  res.end(ruruHTML({ endpoint: '/graphql' }))
 })
+
+app.listen(4000)
+console.log("Api running on: http://localhost:4000")
